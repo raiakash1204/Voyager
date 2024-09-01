f8feda4d1a026692b2d6eee59e0a2d8193df2da2
akash <68575512+raiakash1204@users.noreply.github.com>
2024-09-02 00:17:37 +0530

    created a page for the login page and set it up accordingly.

--- Voyager/Voyager/urls.py
+++ Voyager/Voyager/urls.py
@@ -20,4 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', views.home, name="home"),
+    path('login', views.login, name="login")
 ]
--- Voyager/Voyager/views.py
+++ Voyager/Voyager/views.py
@@ -1,1 +1,10 @@
 #this file has been created by me to define views.
+from django.http import HttpRequest
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "homepage/index.html")
+
+def login(request):
+    return render(request, "login/index.html")
